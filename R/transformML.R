a9065a6d18c88a8082b84f547b25df48d4f63291
nlemeur@fhcrc.org <nlemeur@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-05-26 18:41:03 +0000

    Work on transformML. Fixes still needed
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowUtils@25004 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transformML.R
+++ R/transformML.R
@@ -1,27 +1,118 @@
-## read.transformML("/home/nlemeur/proj/bioc-madman/Rpacks/flowUtils/inst/xml/linearTramsform.xml")
+w## read.transformML("../inst/xml/linearTramsform.xml")
 
 returnTransforms = function(g,refs) {
-    print(g)
+    
+   tranformationId = g@transformationId
+
     ##If we can add it, return a reference to pick up later
-	if(g@transformationId != "dummyTransform") {
-		refs[[g@transformationId]] = g
-		r = xmlNode("transformReference",attrs=list("ref"=g@transformationId), namespace="transforms")
-		class(r) = c("transforms_transformsReference",class(r))
+	if(tranformationId != "dummyTransform") {
+		refs[[tranformationId]] = g
+		r = xmlNode("transformReference",attrs=list("ref"=tranformationId), namespace="transforms")
+                class(r) = c("transforms_transformsReference",class(r))
 		r
 	} else
-		g	
-}
-  
-transforms.transforms_linear= function(x,refs,...) {
-    ##This can be written more compactly, but later...
-    cn = xmlGetAttr(x, "parameter","")
-    a =  xmlGetAttr(x, "a", 1, as.double)
-    b =  xmlGetAttr(x, "b", 0, as.double)
-   
-    transform = c("a"=a,"b"=b)
-    returnTransforms(linearTransform(transformationId="Linear", transform), refs)
+		g
+       
 }
 
+##Linear transformation
+transforms.transforms_linear= function(x,refs,...) {
+  print("linear")
+  cn = xmlGetAttr(x, "parameter","")
+  a =  xmlGetAttr(x, "a", 1, as.double)
+  b =  xmlGetAttr(x, "b", 0, as.double)
+ 
+  transform = c("a"=a,"b"=b)
+  
+  returnTransforms(linearTransform(transformationId="dummyTransform", transform), refs)
+}
+
+##quadratic transformation
+transforms.transforms_quadratic= function(x,refs,...) {
+  print("quadratic")
+  
+  cn = xmlGetAttr(x, "parameter","")
+  a =  xmlGetAttr(x, "a", 1, as.double)
+  b =  xmlGetAttr(x, "b", 1, as.double)
+  c =  xmlGetAttr(x, "c", 0, as.double)
+
+  transform = c("a"=a,"b"=b, "c"=c)
+  
+  returnTransforms(quadraticTransform(transformationId="dummyTransform", transform), refs)
+}
+
+
+##Ln transformation
+transforms.transforms_ln= function(x,refs,...) {
+  print("ln")
+  cn = xmlGetAttr(x, "parameter","")
+  r =  xmlGetAttr(x, "r", 1, as.double)
+  d =  xmlGetAttr(x, "d", 1, as.double)
+ 
+  transform = c("r"=r,"d"=d)
+  
+  returnTransforms(lnTransform(transformationId="dummyTransform", transform), refs)
+}
+
+##log transformation
+transforms.transforms_log= function(x,refs,...) {
+  print("log")
+  cn = xmlGetAttr(x, "parameter","")
+  logbase =  xmlGetAttr(x, "logbase", 10, as.double)
+  r =  xmlGetAttr(x, "r", 1, as.double)
+  d =  xmlGetAttr(x, "d", 1, as.double)
+ 
+  transform = c("logbase"=logbase, "r"=r,"d"=d)
+  
+  returnTransforms(logTransform(transformationId="dummyTransform", transform), refs)
+}
+
+##logicle transformation
+transforms.transforms_logicle= function(x,refs,...) {
+ print("logicle")
+  ##cn = xmlGetAttr(x, "parameter","")
+  ##b =  xmlGetAttr(x, "b", 1, as.double)
+  ##w =  xmlGetAttr(x, "w", 0, as.double)
+  ##r =  xmlGetAttr(x, "r", 262144, as.double)
+  ##tol =  xmlGetAttr(x, "tol", .Machine$double.eps^0.25, as.double)
+  ##maxit= xmlGetAttr(x, "maxit", 5000, as.integer)
+  res = "not implemented"
+  res
+ ## transform = c("b"= b, "w"= w, "r"=r, "tol"= tol, maxit="maxit")
+ ## returnTransforms(logicleTransform(transformationId="dummyTransform", transform), refs)
+}
+
+
+##Bi-exponential transformation
+transforms.transforms_bi.exponential= function(x,refs,...) {
+ print("bi")
+  cn = xmlGetAttr(x, "parameter","")
+  a =  xmlGetAttr(x, "a", .5, as.double)
+  b =  xmlGetAttr(x, "b", 1,  as.double)
+  c =  xmlGetAttr(x, "c", .5, as.double)
+  d =  xmlGetAttr(x, "d", 1,  as.double)
+  f =  xmlGetAttr(x, "f", 0,  as.double)
+  w =  xmlGetAttr(x, "w", 0,  as.double)
+  tol =  xmlGetAttr(x, "tol", .Machine$double.eps^0.25, as.double)
+  maxit= xmlGetAttr(x, "maxit", 5000, as.integer)
+  
+  transform = c("a"= a, "b"= b,"c"= c,"d"= d,"f"= f,"w"= w, "tol"= tol, maxit="maxit")
+  
+  returnTransforms(biexponentialTransform(transformationId="dummyTransform", transform), refs)
+}
+
+transforms.transforms_hyperlog= function(x,refs,...) {
+ print("hyper")
+  res = "not implemented"
+  res
+}
+
+
+transforms.transforms_split.scale= function(x,refs,...) {
+ print("split")
+  res = "not implemented"
+  res
+}
 
 transforms.XMLNode = function(x,...) paste(xmlName(x), xmlGetAttr(x, "id"), sep=":")
 transforms.default = function(x,...) x
@@ -39,14 +130,16 @@ read.transformML = function(file) {
 	#so that we can use dispatch	
 	x = xmlTreeParse(file,
 		handlers=list(
-		"comment"=function(x,...) NULL,
-		"startElement"=function(x) {
-			cn = paste(make.names(c(xmlNamespace(x),xmlName(x))),collapse="_")
-			class(x) = c(cn,class(x))
-			x
-		}
+                  "comment"=function(x,...) NULL,
+                  "startElement"=function(x) {
+                    cn = paste(make.names(c(xmlNamespace(x),xmlName(x))),collapse="_")
+                    tn = make.names(xmlName(x))
+                    class(x) = c(cn,tn, class(x))
+                    x
+                  }
+                  
 	),asTree=TRUE)
-
+        
         ##A place to stash our transformation ids
 	transform_list = new.env(hash=TRUE)
 	end = FALSE
@@ -54,9 +147,8 @@ read.transformML = function(file) {
        	if(!is(xmlRoot(x),"transforms_Transformation.ML"))
 		stop("Not a transformML Document")
 	ret = xmlChildren(xmlRoot(x))
-        nomen=sapply(ret,xmlGetAttr,"newName")
-        print(ret)
-
+        nomen = sapply(ret,xmlGetAttr,"newName")
+        
         def = sapply(ret, xmlChildren)
         noDef = which(names(def)!= "transformation.pre-defined")
         if(length(noDef) > 0)
@@ -75,7 +167,11 @@ read.transformML = function(file) {
             last_len = new_len
             attempts = attempts + 1
 	}
-       	names(ret) = lapply(ret, slot, "")
+
+        for(i in 1:length(ret)){
+          ret[[i]]@transformationId = nomen[i]}
+
+        names(ret) = nomen 
 	ret
     }
 
