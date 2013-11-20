94eb060dacb8e868a3a9304e8ef50e8ed37fdebf
jspidlen@bccrc.ca <jspidlen@bccrc.ca@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-11-20 01:03:44 +0000

    Initial work in progress on the write.gatingML function.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowUtils@83428 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/writeGatingML.R
+++ R/writeGatingML.R
@@ -4,6 +4,9 @@
 
 write.gatingML <- function(file, flowEnv)
 {
+	# TODO Drop this note when done.
+	cat("Please do not use this function, it is currently under development!!\n--------------------------------------------------------------------\n\n")
+	
     if(is.null(file) || !is(file, "character")) 
         stop("A file with the name of the output file is required.", call. = FALSE)
     if(is.null(flowEnv) || !is.environment(flowEnv))
@@ -36,18 +39,53 @@ write.gatingML <- function(file, flowEnv)
 addRectangleGateNode <- function(gatingMLNode, x, flowEnv)
 {
     cat(paste("Working on ", x, ".\n", sep=""))
-    attrs = c("gating:id" = xmlNameToID(x))
+    attrs = c("gating:id" = filterIdtoXMLId(flowEnv[[x]]@filterId))
     gatingMLNode$addNode("gating:RectangleGate", attrs = attrs, close = FALSE)
     for (i in 1:length(flowEnv[[x]]@parameters))
     {
-        min = flowEnv[[x]]@min[i]
-        max = flowEnv[[x]]@max[i]
+        min = flowEnv[[x]]@min[[i]]
+        max = flowEnv[[x]]@max[[i]]
+		parameter = flowEnv[[x]]@parameters[[i]]
+		
         attrs = c()
         if(min != -Inf) attrs = c(attrs, "gating:min" = min)
         if(max != Inf) attrs = c(attrs, "gating:max" = max)
-        # TODO add comensation_ref and transforms as appropriate
+		
+		# TODO Refactor this piece of code
+		if(is(parameter, "transformReference")) parameter = eval(parameter)
+		if(is(parameter, "unitytransform")) attrs = c(attrs, "gating:compensation-ref" = "uncompensated")
+        else if(is(parameter, "singleParameterTransform"))
+		{
+			attrs = c(attrs, "gating:transformation-ref" = parameter@transformationId)
+			parameter = parameter@parameters
+			if(is(parameter, "transformReference")) parameter = eval(parameter)
+			
+			if(is(parameter, "unitytransform")) attrs = c(attrs, "gating:compensation-ref" = "uncompensated")
+			else if(is(parameter, "compensatedParameter"))
+			{
+				if (parameter@spillRefId != "SpillFromFCS") 
+					attrs = c(attrs, "gating:compensation-ref" = filterIdtoXMLId(parameter@spillRefId))
+				else 
+					attrs = c(attrs, "gating:compensation-ref" = "FCS")
+				parameter = parameter@parameters
+			}
+				
+		} 
+		else if(is(parameter, "compensatedParameter"))
+		{
+			if (parameter@spillRefId != "SpillFromFCS") 
+				attrs = c(attrs, "gating:compensation-ref" = filterIdtoXMLId(parameter@spillRefId))
+			else 
+				attrs = c(attrs, "gating:compensation-ref" = "FCS")
+			parameter = parameter@parameters
+		} 
+		else
+		{
+			cat(paste("This should not happen; unexpected parameter class", class(parameter))) # TODO
+		}
+		
         gatingMLNode$addNode("gating:dimension", attrs = attrs, close = FALSE)
-        addDimensionContents(gatingMLNode, flowEnv[[x]]@parameters[i], flowEnv)
+        addDimensionContents(gatingMLNode, parameter, flowEnv)
         gatingMLNode$closeTag()
         
     }
@@ -56,13 +94,16 @@ addRectangleGateNode <- function(gatingMLNode, x, flowEnv)
 
 addDimensionContents <- function(gatingMLNode, parameter, flowEnv)
 {
-    # TODO
-    attrs = c("data-type:name" = "PE-A")
+	if(is(parameter, "unitytransform")) attrs = c("data-type:name" = parameter@parameters)
+	else if(is(parameter, "character")) attrs = c("data-type:name" = parameter)
+	else {
+		cat("\nThis should not happen!\n\n") # TODO
+		attrs = c("data-type:name" = "????")
+	}
     gatingMLNode$addNode("data-type:fcs-dimension", attrs = attrs)
-    dim
 }
 
-xmlNameToID <- function(x)
+filterIdtoXMLId <- function(x)
 {
     # TODO figure out something safe
     x
